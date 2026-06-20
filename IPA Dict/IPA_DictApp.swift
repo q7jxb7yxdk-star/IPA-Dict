8595fcbc9142aacf30a391ca20ce57f7683ea792
q7jxb7yxdk-star <q7jxb7yxdk@privaterelay.appleid.com>
2026-06-20 20:42:22 +0800

    Initial Commit

---
+++ IPA Dict/IPA_DictApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPA_DictApp.swift
+//  IPA Dict
+//
+//  Created by Sunny Yu on 20/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct IPA_DictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
